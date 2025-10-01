a670e8194861826afa5874a8414f761c808431a9
faizancydeo <faizanitaly540@gmail.com>
2025-10-01 17:36:37 +0200

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,6 +3,9 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloCydeo");
+        System.out.println("HelloProgrammers");
+        
     }
 
 }
